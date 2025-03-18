c3d0d6e058f1d3978024299e22bd11ccc1a9408d
vicentejrmc <vicente_pc@outlook.com>
2025-03-18 15:40:00 -0300

    Versão inicial

--- ClassificacaoDeTriangulos.ConsoleApp/Program.cs
+++ ClassificacaoDeTriangulos.ConsoleApp/Program.cs
@@ -1,15 +1,44 @@
-﻿namespace ClassificacaoDeTriangulos.ConsoleApp
+﻿using System.Runtime.InteropServices;
+
+namespace ClassificacaoDeTriangulos.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("---------------------------------------");
-            Console.WriteLine("----- Classificação de Triângulos -----");
-            Console.WriteLine("---------------------------------------");
+            while (true)
+            {
+                //Cabeçalho
+                Console.Clear();
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("---- Classificador de Triangulos ----");
+                Console.WriteLine("-------------------------------------");
 
+                //Entrada de dados
+                Console.Write("Informe os Valores do Triângulo");
+                Console.Write("Lado X: ");
+                int ladoX = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadLine();
+                Console.Write("Lado X: ");
+                int ladoY = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Lado X: ");
+                int ladoZ = Convert.ToInt32(Console.ReadLine());
+
+                //Exibição de Valores Digitados
+                Console.WriteLine("-------------------------------------------------");
+                Console.WriteLine("Valores informados:");
+                Console.WriteLine($"Lado X: {ladoX}");
+                Console.WriteLine($"Lado Y: {ladoY}");
+                Console.WriteLine($"Lado Z: {ladoZ}");
+                Console.WriteLine("-------------------------------------------------");
+
+                Console.Write("Deseja Continuar? ");
+                string opecaoContinuar = Console.ReadLine()!.ToUpper();
+
+                if (opecaoContinuar != "S")
+                    break;
+            }
         }
     }
 }
