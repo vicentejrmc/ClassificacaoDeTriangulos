c0a55e162044904118de3b60881ae8be5a14a5a7
vicentejrmc <vicente_pc@outlook.com>
2025-03-18 14:41:47 -0300

    Add project files.

---
+++ ClassificacaoDeTriangulos.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassificacaoDeTriangulos.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("----- Classificação de Triângulos -----");
+            Console.WriteLine("---------------------------------------");
+
+
+            Console.ReadLine();
+        }
+    }
+}
